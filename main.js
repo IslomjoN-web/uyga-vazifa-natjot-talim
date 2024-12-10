8185611c4da9603519e36abb6825699f06d8c002
islomjon <islomanvarov05@gmail.com>
2024-12-10 19:01:55 +0500

    uyga vazifa bajarildi

---
+++ main.js
@@ -0,0 +1,72 @@
+// 4-masala
+// let d = +prompt("d qiymatni kiriting!")
+// console.log("Aylananing deametri " + " " + d * Math.PI);
+
+
+
+// 5-masala
+// let  a = +prompt("a qiymatni kiriting")
+// let V = a * a * a
+// let S = 6 * (a * a)
+// console.log(V);
+// console.log(S);
+
+
+// 6-masala
+// let a = +prompt(" a tomonni kiriting")
+// let b = +prompt("b tomonni kiriting")
+// let c = +prompt("c tomonni kiriting")
+
+// let V = a * b * c
+// let S = 2 * (a * b + b * c + a * c)
+
+// console.log(V)
+// console.log(S)
+
+
+// 7-masala
+// let R = +prompt("R ni kiriting")
+// let L = 2 * Math.PI * R
+// let S = Math.PI * (R * R)
+
+// console.log(L)
+// console.log(S);
+
+// 8-masala
+// let a = +prompt("a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+
+// console.log( (a * b) / 2 ) 
+
+
+// 9-masala sonlar musbat bolishi shart
+// let a = +prompt(" a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+
+// console.log(Math.sqrt(a) * Math.sqrt(b))
+
+// 10-masala
+// let son1 = +prompt("1-sonni kiriting")
+// let son2 = +prompt("2-sonni kiriting")
+
+// console.log(son1 + son2);
+// console.log(son1 * son2);
+// console.log(son1 * son1, son2 * son2)
+
+// 11 - masala
+// let son1 = +prompt("son1ni kiriting")
+// let son2 = +prompt("son2ni kiriting")
+
+// console.log(son1 + son2);
+// console.log(son1 * son2);
+// console.log(Math.abs(son1), Math.abs(son2));
+
+// 12 - masala
+// let a = +prompt("a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+
+// let c = Math.sqrt(a * a) + Math.sqrt(b * b)
+// let P = a + b + c
+
+// console.log(c);
+// console.log(P)
